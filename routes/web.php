41cd8a313dfb8de0e825c4d1415fabb9236dca5c
Araceli Torrejon <aracelihtpull@gmail.com>
2024-11-17 00:04:29 -0400

    ruta de la vista

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ShippingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/shipping-test', [ShippingController::class, 'showForm'])->name('shipping.form');
+Route::post('/shipping-test', [ShippingController::class, 'calculateShipping'])->name('shipping.calculate');
